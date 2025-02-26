6166282e68035020c055760a8897d077c1b35e1b
abdugaffor2004 <abdugafforlutfillaev14@gmail.com>
2025-02-26 21:55:34 +0300

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-import prisma from '@/lib/prisma';
-
 export default async function Home() {
-  const vehicles = await prisma.vehicle.findMany({
-    include: {
-      manufacturer: true,
-      history: true,
-      safety: true,
-      specification: true,
-    },
-  });
-  console.log(vehicles);
-
   return <div>home</div>;
 }
